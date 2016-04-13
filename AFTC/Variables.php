7f0d37c0408f7a84409482dc372f0a91b1466e58
Darcey <Darcey.Lloyd@gmail.com>
2016-04-13 22:24:49 +0100

    0.8 additional
    0.8 missing file added

---
+++ AFTC/Variables.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Author: Darcey@AllForTheCode.co.uk
+ * Date: 13/01/2016
+ *
+ * Config is not the place for your projects variables, this is
+ *
+ */
+
+
+namespace AFTC\Framework\App;
+
+
+class Variables
+{
+	// Required variables
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	// User defined variables
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	public static $maintenance_mode = false;
+
+	public static $browser_title = "AFTC";
+	public static $browser_title_short = "AFTC";
+
+	public static $CookieLoggedInCode = "asf90vjz0xv790as9d80a9sc080as67d097as09zxv";
+	public static $SessionLoggedInCode = "908a0970xzv70asda0s97vzxpojasc97x9c6a9s8d7";
+	public static $PostbackCode = "AFTCPostBackCode336215asad5654as654das";
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	public static function init()
+	{
+
+	}
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
